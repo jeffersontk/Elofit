3796857746b83d1524fd4ec623d9fa0ae46208b5
dev <j.brito@vertexdigital.co>
2021-01-21 23:31:18 -0300

    inciando o toolbar, fata resolver o erro na imagem e finalizar o menu hambuguer

--- components/toolbar.js
+++ components/toolbar.js
@@ -2,9 +2,36 @@ import React from 'react';
 
 function toolbar() {
   return (
-    <div>
-      <h1>Toolbar</h1>
-    </div>
+    <nav className="bg-hero-blur">
+      <div className="max-w-7x1 max-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+            <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-expanded="false">
+              <svg className="block h-6 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+              <svg className="hidden h-6 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-between">
+            <div className="flex-shrink-0 flex items-center">
+              <img className="block lg:hidden h-8 w-auto" src="../assets/Logo_EloFit.svg" alt="Logo-Elofit" />
+              <img className="hidden lg:block h-8 w-auto" src="../assets/logo-elofit.png" alt="Logo-Elofit"/>
+            </div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <a href="#" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Unidades</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Atividades</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Equipe</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contato</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
