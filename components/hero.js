d9355ee691767e7b3b79024b809146046dbef1b6
dev <j.brito@vertexdigital.co>
2021-01-23 17:28:58 -0300

    campo unidades, renomear imagens colando id no hero

--- components/hero.js
+++ components/hero.js
@@ -3,10 +3,10 @@ import React from 'react';
 function hero() {
   return (
     <>
-      <header className="md:bg-hero-pattern-lg bg-hero-pattern-xs bg-auto bg-no-repeat bg-center bg-cover bg-fixed">
+      <header className="md:bg-hero-pattern-lg bg-hero-pattern-xs bg-auto bg-no-repeat bg-center bg-cover bg-fixed" id="hero">
         <div className="bg-hero-blur flex flex-col items-center md:items-start px-4 justify-center h-screen">
           <div className="md:max-w-xl ml-10 mt-40 md:mt-0">
-            <h1 className="text-white md:text-4sm text-3xl  font-regular"> <strong className="text-green-light text-4xl ">Elofit academia para todos <br/> </strong> Venha começar hoje mesmo seu treino.</h1>
+            <h1 className="text-white md:text-4sm text-3xl font-regular"> <strong className="text-green-light text-4xl ">Elofit academia para todos <br/> </strong> Venha começar hoje mesmo seu treino.</h1>
           </div>
           <div className="text-center md:ml-5">
             <div className="flex my-2 p-4 ">
